dfdd6c27fd07aed76c997fe7ff35ad759aa28176
Louis6785 <louis_shi@jabil.com>
2017-03-01 16:52:04 +0800

    改用Express.Router()

--- app.js
+++ app.js
@@ -8,12 +8,15 @@ var mongodb = require('./database/connectMongoDB');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var photos = require('./routes/photos');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.set('photos', __dirname + '/public/photos');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -26,6 +29,7 @@ app.use('/lib', express.static(path.join(__dirname, 'node_modules')))
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/photos', photos);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
